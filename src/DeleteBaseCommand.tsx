b143f7341f9ed39caf2195f4f3cb832d8c0d20bb
Martin Helmich <m.helmich@mittwald.de>
2023-07-11 16:47:03 +0200

    Rework DeleteBaseCommand

--- src/DeleteBaseCommand.tsx
+++ src/DeleteBaseCommand.tsx
@@ -1,5 +1,10 @@
 import { Command, Flags, Interfaces, ux } from "@oclif/core";
 import { BaseCommand } from "./BaseCommand.js";
+import { ExecRenderBaseCommand } from "./rendering/react/ExecRenderBaseCommand.js";
+import { makeProcessRenderer, processFlags } from "./rendering/react/process_flags.js";
+import { Success } from "./rendering/react/components/Success.js";
+import { Text } from "ink";
+import React from "react";
 
 export type DeleteFlags<T extends typeof Command> = Interfaces.InferredFlags<
   (typeof DeleteBaseCommand)["baseFlags"] & T["flags"]
@@ -9,32 +14,17 @@ export type DeleteArgs<T extends typeof Command> = Interfaces.InferredArgs<
 >;
 
 export abstract class DeleteBaseCommand<
-  T extends typeof Command,
-> extends BaseCommand<T> {
+  T extends typeof BaseCommand,
+> extends ExecRenderBaseCommand<T, void> {
   static resourceName = "object";
   static baseFlags = {
+    ...processFlags,
     force: Flags.boolean({
       description: "Do not ask for confirmation",
     }),
   };
 
-  protected flags!: DeleteFlags<T>;
-  protected args!: DeleteArgs<T>;
-
-  public async init(): Promise<void> {
-    await super.init();
-    const { args, flags } = await this.parse({
-      flags: this.ctor.flags,
-      baseFlags: (super.ctor as typeof BaseCommand).baseFlags,
-      args: this.ctor.args,
-      strict: this.ctor.strict,
-    });
-
-    this.args = args as DeleteArgs<T>;
-    this.flags = flags as DeleteFlags<T>;
-  }
-
-  public async run(): Promise<void> {
+  protected async exec(): Promise<void> {
     const resourceName = (this.ctor as typeof DeleteBaseCommand).resourceName;
 
     if (!this.flags.force) {
@@ -48,11 +38,17 @@ export abstract class DeleteBaseCommand<
       }
     }
 
-    ux.action.start(`deleting ${resourceName}`);
+    const process = makeProcessRenderer(this.flags as any, `Deleting ${resourceName}`);
+    const deletingStep = process.addStep(<Text>deleting {resourceName}</Text>);
 
     await this.deleteResource();
 
-    ux.action.stop();
+    deletingStep.complete();
+    process.complete(<Success>The {resourceName} was successfully deleted. How sad. 🥺</Success>);
+  }
+
+  protected render(executionResult: void): React.ReactNode {
+    return undefined;
   }
 
   protected abstract deleteResource(): Promise<void>;
