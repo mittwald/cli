6065fdc6a3369653670964dd4afe81f04af6d977
Martin Helmich <m.helmich@mittwald.de>
2023-07-13 14:53:03 +0200

    Revamp "filesystem usage" command

---
+++ src/lib/bytes.ts
@@ -0,0 +1,19 @@
+export function parseBytes(input: string): number {
+  const numeric = parseInt(input.replace(/[^0-9]/g, ''), 10);
+
+  if (`${numeric}` == input) {
+    return numeric;
+  }
+
+  if (input.toLowerCase().endsWith("gi")) {
+    return numeric * (1 << 30);
+  }
+  if (input.toLowerCase().endsWith("mi")) {
+    return numeric * (1 << 20);
+  }
+  if (input.toLowerCase().endsWith("ki")) {
+    return numeric * (1 << 10);
+  }
+
+  throw new Error("unsupported byte unit; supported are 'gi', 'mi', 'ki'");
+}
--- src/rendering/react/components/Note.tsx
+++ src/rendering/react/components/Note.tsx
@@ -5,17 +5,21 @@ type NoteProps = PropsWithChildren<{
   title?: string;
   color?: string;
   marginY?: number;
+  raw?: boolean;
 }>
 
+export const noteColor = "#407FF8";
+
 export const Note: FC<NoteProps> = (props) => {
-  const { title = "Note", color = "#407FF8", marginY = 0 } = props;
+  const { title = "Note", color = noteColor, marginY = 0, raw = false } = props;
+  const contents = raw ? props.children : <Text wrap="wrap" color={color}>{props.children}</Text>;
 
   return (
     <Box width={80} borderStyle={"round"} borderColor={color} flexDirection="column" paddingX={2} marginY={marginY}>
       <Text bold underline color={color}>
         {title.toUpperCase()}
       </Text>
-      <Text wrap="wrap" color={color}>{props.children}</Text>
+      {contents}
     </Box>
   );
 };
