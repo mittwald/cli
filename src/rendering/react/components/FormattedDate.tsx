4eb0e06df4b0e1a493fa58116964dba8d726d731
Marco Falkenberg <m.falkenberg@mittwald.de>
2023-07-13 17:23:46 +0200

    Fix combined date not shwoing

--- src/rendering/react/components/FormattedDate.tsx
+++ src/rendering/react/components/FormattedDate.tsx
@@ -26,7 +26,7 @@ export const FormattedDate: FC<Props> = (props) => {
   if (relativeEl && absoluteEl) {
     return (
       <Text>
-        {relative} ({absolute})
+        {relativeEl} ({absoluteEl})
       </Text>
     );
   }
