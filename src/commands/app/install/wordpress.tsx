40cd52dcd3e3fd2c9dbc116d3a25d509c40bf6c1
mhopfner <m.hopfner@mittwald.de>
2023-07-22 15:40:35 +0200

    add autofill for wordpress installation - makes every flag optional.

--- src/commands/app/install/wordpress.tsx
+++ src/commands/app/install/wordpress.tsx
@@ -18,6 +18,7 @@ import { Success } from "../../../rendering/react/components/Success.js";
 import { waitUntil } from "../../../lib/wait.js";
 import React from "react";
 import AppAppVersion = MittwaldAPIV2.Components.Schemas.AppAppVersion;
+import { getProjectShortIdFromUuid } from "../../../lib/project/shortId.js";
 
 export default class AppCreateWordpress extends ExecRenderBaseCommand<
   typeof AppCreateWordpress,
@@ -34,24 +35,24 @@ export default class AppCreateWordpress extends ExecRenderBaseCommand<
       default: "latest",
     }),
     host: Flags.string({
-      required: true,
+      required: false,
       description:
         "Host under which the App will be available (Needs to be created separately).",
     }),
     "admin-user": Flags.string({
-      required: true,
+      required: false,
       description: "First Admin User for the app.",
     }),
     "admin-email": Flags.string({
-      required: true,
+      required: false,
       description: "First Admin Users E-Mail.",
     }),
     "admin-pass": Flags.string({
-      required: true,
+      required: false,
       description: "First Admin Users Password.",
     }),
     "site-title": Flags.string({
-      required: true,
+      required: false,
       description: "Site Title of the created appInstallation.",
     }),
     wait: Flags.boolean({
@@ -62,7 +63,6 @@ export default class AppCreateWordpress extends ExecRenderBaseCommand<
 
   protected async exec(): Promise<{ appInstallationId: string }> {
     const process = makeProcessRenderer(this.flags, "Installing WordPress");
-
     const { flags, args } = await this.parse(AppCreateWordpress);
     const app = "WordPress";
     const appUuid: string = await getAppUuidFromAppName(this.apiClient, app);
@@ -72,6 +72,69 @@ export default class AppCreateWordpress extends ExecRenderBaseCommand<
       args,
       this.config,
     );
+
+    if (
+      flags.host &&
+      flags["site-title"] &&
+      flags["admin-user"] &&
+      flags["admin-email"] &&
+      flags["admin-pass"]
+    ) {
+      // all optional flags given, no autofill necessary
+    } else {
+      const ownUser = await this.apiClient.user.getOwnAccount();
+      assertStatus(ownUser, 200);
+
+      if (!flags.host) {
+        flags.host = "https://" + projectId + ".project.space";
+      }
+
+      if (!flags["site-title"] && projectId) {
+        flags["site-title"] =
+          "Wordpress " +
+          (await getProjectShortIdFromUuid(this.apiClient, projectId));
+      }
+
+      if (!flags["admin-user"]) {
+        if (ownUser.data.person) {
+          flags["admin-user"] =
+            ownUser.data.person.firstName.charAt(0).toLowerCase() +
+            ownUser.data.person.lastName.toLowerCase();
+        } else {
+          flags["admin-user"] = require("os")
+            .userInfo()
+            .username.replace(/[ ]/g, "-")
+            .toLowerCase();
+        }
+        process.addInfo(
+          <Text>
+            Generated Admin User: <Value>{flags["admin-user"]}</Value>
+          </Text>,
+        );
+      }
+
+      if (!flags["admin-pass"]) {
+        flags["admin-pass"] = Math.random().toString(36).slice(-16);
+        process.addInfo(
+          <Text>
+            Generated Admin Pass: <Value>{flags["admin-pass"]}</Value>
+          </Text>,
+        );
+      }
+
+      if (!flags["admin-email"]) {
+        if (ownUser.data.email) {
+          flags["admin-email"] = ownUser.data.email;
+        } else {
+          flags["admin-email"] = projectId + "@project.space";
+        }
+        process.addInfo(
+          <Text>
+            Used Admin Email: <Value>{flags["admin-email"]}</Value>
+          </Text>,
+        );
+      }
+    }
 
     const appVersion = await process.runStep(
       "determining app version",
@@ -109,14 +172,14 @@ export default class AppCreateWordpress extends ExecRenderBaseCommand<
           pathParameters: { projectId },
           data: {
             appVersionId: appVersion.id,
-            description: flags["site-title"],
+            description: flags["site-title"] as string,
             updatePolicy: "none",
             userInputs: [
-              { name: "host", value: flags.host },
-              { name: "site_title", value: flags["site-title"] },
-              { name: "admin_user", value: flags["admin-user"] },
-              { name: "admin_email", value: flags["admin-email"] },
-              { name: "admin_pass", value: flags["admin-pass"] },
+              { name: "host", value: flags.host as string },
+              { name: "site_title", value: flags["site-title"] as string },
+              { name: "admin_user", value: flags["admin-user"] as string },
+              { name: "admin_email", value: flags["admin-email"] as string },
+              { name: "admin_pass", value: flags["admin-pass"] as string },
             ],
           },
         });
