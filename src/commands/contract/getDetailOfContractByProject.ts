eaa2bc6b742df43684363731ffe9b9edbc17d58c
Martin Helmich <m.helmich@mittwald.de>
2023-07-27 08:42:13 +0200

    Remove contract commands (re-build when necessary)

--- src/commands/contract/getDetailOfContractByProject.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-/* prettier-ignore */
-/* This file is auto-generated with acg (@mittwald/api-code-generator) */
-import {
-  GeneratedContractGetDetailOfContractByProject
-} from "../../generated/contract/getDetailOfContractByProject.js";
-
-export default class Get extends GeneratedContractGetDetailOfContractByProject {}
