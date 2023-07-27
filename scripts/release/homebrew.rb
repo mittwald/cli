2c19bd8fad4c172b073a38b1d52317d51a0ff7d5
Martin Helmich <m.helmich@mittwald.de>
2023-07-27 18:04:31 +0200

    Automate homebrew releases

---
+++ scripts/release/homebrew.rb
@@ -0,0 +1,25 @@
+class Mw < Formula
+  desc "The mittwald command-line tool"
+  homepage "https://github.com/mittwald/cli"
+  url "https://mittwald-cli.s3.eu-central-1.amazonaws.com/versions/__VERSION__/__VERSIONHASH__/mw-v__VERSION__-__VERSIONHASH__-darwin-x64.tar.xz"
+  sha256 "__SHA_X64__"
+  version "__VERSION__"
+  version_scheme 1
+
+  on_macos do
+    if Hardware::CPU.arm?
+  url "https://mittwald-cli.s3.eu-central-1.amazonaws.com/versions/__VERSION__/__VERSIONHASH__/mw-v__VERSION__-__VERSIONHASH__-darwin-arm64.tar.xz"
+      sha256 "__SHA_ARM64__"
+    end
+  end
+
+  def install
+    inreplace "bin/mw", /^CLIENT_HOME=/, "export HEROKU_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"bin/mw"
+  end
+
+  test do
+    system bin/"mw", "--version"
+  end
+end
