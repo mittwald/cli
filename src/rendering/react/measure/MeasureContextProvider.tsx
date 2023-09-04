af48bbb2f9ae6530040702f8c0ac67e503e7bd97
Marco Falkenberg <m.falkenberg@mittwald.de>
2023-09-04 10:33:15 +0200

    Hide table during measuring dimensions

--- src/rendering/react/measure/MeasureContextProvider.tsx
+++ src/rendering/react/measure/MeasureContextProvider.tsx
@@ -1,5 +1,11 @@
 import { Box } from "ink";
-import { FC, PropsWithChildren, useRef } from "react";
+import {
+  FC,
+  PropsWithChildren,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
 import { createMeasureContext, measureContext } from "./context.js";
 import { MeasureRenderer } from "./MeasureRenderer.js";
 
@@ -8,13 +14,24 @@ export const MeasureContextProvider: FC<PropsWithChildren> = (props) => {
 
   const context = useRef(createMeasureContext()).current;
 
+  const [isMeasuring, setIsMeasuring] = useState(true);
+
+  useLayoutEffect(() => {
+    setIsMeasuring(false);
+  }, []);
+
   return (
     <measureContext.Provider value={context}>
       <Box flexDirection="column" gap={1}>
         <Box>
           <MeasureRenderer />
         </Box>
-        <Box>{children}</Box>
+        <Box
+          height={isMeasuring ? 0 : undefined}
+          overflow={isMeasuring ? "hidden" : undefined}
+        >
+          {children}
+        </Box>
       </Box>
     </measureContext.Provider>
   );
