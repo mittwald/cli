a16967ebca596f98f2b1eccdc0c0e73af9005083
Martin Helmich <m.helmich@mittwald.de>
2023-10-18 10:13:38 +0200

    Clean up cronjob commands

--- src/commands/cronjob/execution/list.ts
+++ src/commands/cronjob/execution/list.ts
@@ -1,21 +1,23 @@
 import { Simplify } from "@mittwald/api-client-commons";
 import { MittwaldAPIV2, MittwaldAPIV2Client } from "@mittwald/api-client";
-import { SuccessfulResponse } from "../../../../types.js";
-import { ListColumns } from "../../../../Formatter.js";
-import { ListBaseCommand } from "../../../../ListBaseCommand.js";
+import { SuccessfulResponse } from "../../../types.js";
+import { ListColumns } from "../../../Formatter.js";
+import { ListBaseCommand } from "../../../ListBaseCommand.js";
 import { Flags } from "@oclif/core";
-import { formatRelativeDate } from "../../../../lib/viewhelpers/date.js";
+import { formatRelativeDate } from "../../../lib/viewhelpers/date.js";
 
 type ResponseItem = Simplify<
   MittwaldAPIV2.Paths.V2CronjobsCronjobIdExecutions.Get.Responses.$200.Content.ApplicationJson[number]
 >;
-export type Response = Awaited<
+type Response = Awaited<
   ReturnType<MittwaldAPIV2Client["cronjob"]["listExecutions"]>
 >;
 
 export class List extends ListBaseCommand<typeof List, ResponseItem, Response> {
   static description = "List CronjobExecutions belonging to a Cronjob.";
 
+  static aliases = ["project:cronjob:execution:list"];
+  static deprecateAliases = true;
   static args = {};
   static flags = {
     ...ListBaseCommand.baseFlags,
