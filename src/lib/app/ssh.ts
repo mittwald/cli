40c8a00599a0bc386cccf2e96a61c12422673a9f
Martin Helmich <m.helmich@mittwald.de>
2024-01-24 17:52:18 +0100

    Extract SSH connection data into function

---
+++ src/lib/app/ssh.ts
@@ -0,0 +1,46 @@
+import { MittwaldAPIV2Client, assertStatus } from "@mittwald/api-client";
+import path from "path";
+
+export interface SSHConnectionData {
+  host: string;
+  user: string;
+  directory: string;
+}
+
+export async function getSSHConnectionForAppInstallation(
+  client: MittwaldAPIV2Client,
+  appInstallationId: string,
+): Promise<SSHConnectionData> {
+  const appInstallationResponse = await client.app.getAppinstallation({
+    appInstallationId,
+  });
+
+  assertStatus(appInstallationResponse, 200);
+
+  if (appInstallationResponse.data.projectId === undefined) {
+    throw new Error("Project ID of app must not be undefined");
+  }
+
+  const projectResponse = await client.project.getProject({
+    projectId: appInstallationResponse.data.projectId,
+  });
+
+  assertStatus(projectResponse, 200);
+
+  const userResponse = await client.user.getOwnAccount();
+
+  assertStatus(userResponse, 200);
+
+  const host = `ssh.${projectResponse.data.clusterID}.${projectResponse.data.clusterDomain}`;
+  const user = `${userResponse.data.email}@${appInstallationResponse.data.shortId}`;
+  const directory = path.join(
+    projectResponse.data.directories["Web"],
+    appInstallationResponse.data.installationPath,
+  );
+
+  return {
+    host,
+    user,
+    directory,
+  };
+}
