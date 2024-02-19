d8124ec55d738f5e2027b5bb176a1fb94eef25f5
Martin Helmich <m.helmich@mittwald.de>
2024-02-19 18:47:44 +0100

    Cleanup "ddev init" command

--- src/commands/ddev/init.tsx
+++ src/commands/ddev/init.tsx
@@ -12,8 +12,9 @@ import yaml from "js-yaml";
 import { spawnInProcess } from "../../rendering/process/process_exec.js";
 import { Flags } from "@oclif/core";
 import { DDEVInitSuccess } from "../../rendering/react/components/DDEV/DDEVInitSuccess.js";
-import { ddevConfigToFlags } from "../../lib/ddev/config.js";
+import { DDEVConfig, ddevConfigToFlags } from "../../lib/ddev/config.js";
 import { hasBinary } from "../../lib/hasbin.js";
+import { ProcessRenderer } from "../../rendering/process/process.js";
 
 export class Init extends ExecRenderBaseCommand<typeof Init, void> {
   static summary = "Initialize a new ddev project in the current directory";
@@ -43,59 +44,14 @@ export class Init extends ExecRenderBaseCommand<typeof Init, void> {
     const appInstallationId = await this.withAppInstallationId(Init);
     const r = makeProcessRenderer(this.flags, "Initializing DDEV project");
 
-    const { "override-mittwald-plugin": mittwaldPlugin } = this.flags;
-    let { "project-name": projectName } = this.flags;
+    await assertDDEVIsInstalled(r);
 
-    await r.runStep("check if DDEV is installed", async () => {
-      if (!(await hasBinary("ddev"))) {
-        throw new Error("this command requires DDEV to be installed");
-      }
-    });
+    const config = await this.writeMittwaldConfiguration(r, appInstallationId);
+    const projectName = await this.determineProjectName(r);
 
-    const config = await r.runStep(
-      "creating mittwald-specific DDEV configuration",
-      async () => {
-        const builder = new DDEVConfigBuilder(this.apiClient);
-        const config = await builder.build(appInstallationId, "auto");
-
-        await mkdir(".ddev", { recursive: true });
-        await writeFile(
-          path.join(".ddev", "config.mittwald.yaml"),
-          yaml.dump(config),
-        );
-
-        return config;
-      },
-    );
-
-    if (!projectName) {
-      projectName = await r.addInput("Enter the project name", false);
-    }
-
-    const ddevArgs = [
-      "--project-name",
-      projectName,
-      ...ddevConfigToFlags(config),
-    ];
-
-    if (config.type) {
-      ddevArgs.push("--project-type", config.type);
-    }
-
-    await spawnInProcess(r, "initializing DDEV project", "ddev", [
-      "config",
-      ...ddevArgs,
-    ]);
-
-    await spawnInProcess(r, "installing mittwald plugin", "ddev", [
-      "get",
-      mittwaldPlugin,
-    ]);
-
-    await spawnInProcess(r, "adding SSH credentials to DDEV", "ddev", [
-      "auth",
-      "ssh",
-    ]);
+    await this.initializeDDEVProject(r, config, projectName);
+    await this.installMittwaldPlugin(r);
+    await this.addSSHCredentials(r);
 
     await r.complete(<DDEVInitSuccess />);
   }
@@ -103,4 +59,74 @@ export class Init extends ExecRenderBaseCommand<typeof Init, void> {
   protected render(): React.ReactNode {
     return undefined;
   }
+
+  private async addSSHCredentials(r: ProcessRenderer) {
+    await spawnInProcess(r, "adding SSH credentials to DDEV", "ddev", [
+      "auth",
+      "ssh",
+    ]);
+  }
+
+  private async installMittwaldPlugin(r: ProcessRenderer) {
+    const { "override-mittwald-plugin": mittwaldPlugin } = this.flags;
+    await spawnInProcess(r, "installing mittwald plugin", "ddev", [
+      "get",
+      mittwaldPlugin,
+    ]);
+  }
+
+  private async initializeDDEVProject(
+    r: ProcessRenderer,
+    config: Partial<DDEVConfig>,
+    projectName: string,
+  ): Promise<void> {
+    await spawnInProcess(r, "initializing DDEV project", "ddev", [
+      "config",
+      "--project-name",
+      projectName,
+      ...ddevConfigToFlags(config),
+    ]);
+  }
+
+  private async determineProjectName(r: ProcessRenderer): Promise<string> {
+    const { "project-name": projectName } = this.flags;
+    if (projectName) {
+      return projectName;
+    }
+
+    return await r.addInput("Enter the project name", false);
+  }
+
+  private async writeMittwaldConfiguration(
+    r: ProcessRenderer,
+    appInstallationId: string,
+  ) {
+    return await r.runStep(
+      "creating mittwald-specific DDEV configuration",
+      async () => {
+        const builder = new DDEVConfigBuilder(this.apiClient);
+        const config = await builder.build(appInstallationId, "auto");
+        const configFile = path.join(".ddev", "config.mittwald.yaml");
+
+        await writeYAMLToFile(configFile, config);
+
+        return config;
+      },
+    );
+  }
+}
+
+async function assertDDEVIsInstalled(r: ProcessRenderer): Promise<void> {
+  await r.runStep("check if DDEV is installed", async () => {
+    if (!(await hasBinary("ddev"))) {
+      throw new Error("this command requires DDEV to be installed");
+    }
+  });
+}
+
+async function writeYAMLToFile(filename: string, data: unknown): Promise<void> {
+  const dirname = path.dirname(filename);
+
+  await mkdir(dirname, { recursive: true });
+  await writeFile(filename, yaml.dump(data));
 }
