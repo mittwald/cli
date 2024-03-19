4044a2cbab0930144f4a75c0832b76b6e7660eb7
Michel Hopfner <m.hopfner@mittwald.de>
2024-03-19 09:51:48 +0100

    Add PrestaShop, NEOS, Moodle

---
+++ src/commands/app/install/moodle.tsx
@@ -0,0 +1,41 @@
+import { ExecRenderBaseCommand } from "../../../rendering/react/ExecRenderBaseCommand.js";
+import React from "react";
+import {
+  AppInstallationResult,
+  AppInstaller,
+} from "../../../lib/app/Installer.js";
+
+export const moodleInstaller = new AppInstaller(
+  "5ba3b4ea-9f79-460a-bbef-d901beca4cf1",
+  "Moodle",
+  [
+    "version",
+    "host",
+    "admin-user",
+    "admin-email",
+    "admin-pass",
+    "site-title",
+    "wait",
+  ] as const,
+);
+
+export default class InstallMoodle extends ExecRenderBaseCommand<
+  typeof InstallMoodle,
+  AppInstallationResult
+> {
+  static description = moodleInstaller.description;
+  static flags = moodleInstaller.flags;
+
+  protected async exec(): Promise<{ appInstallationId: string }> {
+    return moodleInstaller.exec(
+      this.apiClient,
+      this.args,
+      this.flags,
+      this.config,
+    );
+  }
+
+  protected render(result: AppInstallationResult): React.ReactNode {
+    return moodleInstaller.render(result, this.flags);
+  }
+}
