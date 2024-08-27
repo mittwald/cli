f63b8a20067683517124ea795cfdc4bf18a88603
Michel Hopfner <m.hopfner@mittwald.de>
2024-08-27 16:13:16 +0200

    Set useful summaries and descriptions for helptext generation.

--- src/lib/resources/cronjob/flags.tsx
+++ src/lib/resources/cronjob/flags.tsx
@@ -2,28 +2,45 @@ import { Flags } from "@oclif/core";
 
 export const cronjobFlagDefinitions = {
   description: Flags.custom<string>({
-    description: "Set cron job description",
+    summary: "Set cron job description.",
+    description:
+      "This will be displayed as the cron jobs 'name' of the cron job in mStudio.",
   }),
   interval: Flags.custom<string>({
-    description: "Set cron job execution interval",
+    summary: "Set the interval for cron jobs to run.",
+    description:
+      "Must be specified as a cron schedule expression. Defines the interval at which the cron job will be executed.",
   }),
   active: Flags.custom<boolean>({
-    description: "Set whether automatic execution is active",
+    summary: "Specify whether automatic execution is active.",
+    description:
+      "Requires a boolean value (true/false) to enable or disable automatic execution for the cron job.",
   }),
   email: Flags.custom<string>({
-    description: "Set target email to send error messages to",
+    summary: "Set the target email to which error messages will be sent.",
+    description:
+      "If a cron job fails, a detailed error message will be sent to this email address.",
   }),
   timeout: Flags.custom<number>({
-    description: "Set timeout in seconds after wich the process is killed",
+    summary: "Set the timeout after which the process will be killed.",
+    description:
+      "The timeout, given in seconds, defines the amount of time after which a running cron job will be killed. If defined an email address is defined, an error message will be sent to the specified email address.",
   }),
   url: Flags.custom<string>({
-    description: "Set url to use on cron job execution",
+    summary: "Set the URL to use when running a cron job.",
+    description:
+      "Define a URL with protocol to which a request will be dispatched when the cron job is executed. For example: 'https://my-website.com/cron-job'. Not required if a command and interpreter is defined.",
   }),
   command: Flags.custom<string>({
-    description: "Set file and parameters to execute on cron job execution",
+    summary:
+      "Specify the file and arguments to be executed when the cron job is run.",
+    description:
+      " Specifies a file to be executed with the specified interpreter. Additional arguments can be appended to the command to be passed to the script. Not required if a URL is given.",
   }),
   interpreter: Flags.custom<string>({
-    description: "Set interpreter to use for execution",
     options: ["bash", "php"],
+    summary: "Set the interpreter to be used for execution.",
+    description:
+      "Must be either 'bash' or 'php'. Define the interpreter to be used to execute the previously defined command. The interpreter should match the corresponding command or script.",
   }),
 };
