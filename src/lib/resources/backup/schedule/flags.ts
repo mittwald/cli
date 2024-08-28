8595b71d270158312d54e7792b7ad5156806505b
Michel Hopfner <m.hopfner@mittwald.de>
2024-08-28 14:26:17 +0200

    set useful summaries and descriptions for backup schedule flags

--- src/lib/resources/backup/schedule/flags.ts
+++ src/lib/resources/backup/schedule/flags.ts
@@ -2,13 +2,20 @@ import { Flags } from "@oclif/core";
 
 export const backupScheduleFlagDefinitions = {
   description: Flags.custom<string>({
-    description: "Set the description for the backup schedule",
+    summary: "Set the description for the backup schedule.",
+    description:
+      "Set the description for the given backup schedule to be displayed in mStudio and with the list command.",
   }),
   schedule: Flags.custom<string>({
-    description: "Define the schedule itself",
+    summary: "Set the interval at which the backup should be scheduled.",
+    description:
+      "Must be specified as a cron schedule expression. " +
+      "Cannot be scheduled more often than once per hour. " +
+      "Defines the interval at which the backup creation will be executed.",
   }),
   ttl: Flags.custom<string>({
+    summary: "Define the backup retention period in days for backups created.",
     description:
-      "Define the backup storage period in days, for through this schedule created backups",
+      "Must be specified as a natural number between 7 and 400, representing the number of days the backup will be kept.",
   }),
 };
