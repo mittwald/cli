8a0c717921adff58804e2b1a4052a9010f6a5da0
Martin Helmich <m.helmich@mittwald.de>
2024-10-11 11:13:08 +0200

    Add commands to manage extensions

---
+++ src/commands/extension/list.ts
@@ -0,0 +1,33 @@
+import { Simplify } from "@mittwald/api-client-commons";
+import { MittwaldAPIV2, MittwaldAPIV2Client } from "@mittwald/api-client";
+import { ListBaseCommand } from "../../lib/basecommands/ListBaseCommand.js";
+import { ListColumns } from "../../rendering/formatter/Table.js";
+
+type ResponseItem = Simplify<
+  MittwaldAPIV2.Paths.V2Extensions.Get.Responses.$200.Content.ApplicationJson[number]
+>;
+export type PathParams = MittwaldAPIV2.Paths.V2Extensions.Get.Parameters.Path;
+export type Response = Awaited<
+  ReturnType<MittwaldAPIV2Client["marketplace"]["extensionListExtensions"]>
+>;
+
+export class List extends ListBaseCommand<typeof List, ResponseItem, Response> {
+  static description = "Get all available extensions.";
+
+  static args = {};
+  static flags = {
+    ...ListBaseCommand.baseFlags,
+  };
+
+  public async getData(): Promise<Response> {
+    return await this.apiClient.marketplace.extensionListExtensions();
+  }
+
+  protected getColumns(data: ResponseItem[]): ListColumns<ResponseItem> {
+    const { id } = super.getColumns(data, {});
+    return {
+      id,
+      name: {},
+    };
+  }
+}
---
+++ src/commands/extension/uninstall.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { ExecRenderBaseCommand } from "../../lib/basecommands/ExecRenderBaseCommand.js";
+import {
+  makeProcessRenderer,
+  processFlags,
+} from "../../rendering/process/process_flags.js";
+import { Args } from "@oclif/core";
+import assertSuccess from "../../lib/apiutil/assert_success.js";
+import { Success } from "../../rendering/react/components/Success.js";
+
+type UninstallResult = {};
+
+export default class Uninstall extends ExecRenderBaseCommand<
+  typeof Uninstall,
+  UninstallResult
+> {
+  static description = "Remove an extension from an organization";
+
+  static flags = {
+    ...processFlags,
+  };
+
+  static args = {
+    "extension-instance-id": Args.string({
+      description: "the ID of the extension instance to uninstall",
+      required: true,
+    }),
+  };
+
+  protected async exec(): Promise<UninstallResult> {
+    const { "extension-instance-id": extensionInstanceId } = this.args;
+
+    const p = makeProcessRenderer(this.flags, "Uninstalling extension");
+
+    await p.runStep("removing extension instance", async () => {
+      const resp =
+        await this.apiClient.marketplace.extensionDeleteExtensionInstance({
+          extensionInstanceId,
+        });
+
+      assertSuccess(resp);
+    });
+
+    await p.complete(<Success>Extension successfully uninstalled</Success>);
+
+    return {};
+  }
+
+  protected render(): React.ReactNode {
+    return undefined;
+  }
+}
