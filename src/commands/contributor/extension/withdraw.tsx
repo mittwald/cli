a381096d96a923a6735dd38feb2826738fa08885
Martin Helmich <m.helmich@mittwald.de>
2025-04-23 16:21:36 +0200

    feat: add confirmation (and --force flag) to withdraw command

--- src/commands/contributor/extension/withdraw.tsx
+++ src/commands/contributor/extension/withdraw.tsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { FC } from "react";
 import { ExecRenderBaseCommand } from "../../../lib/basecommands/ExecRenderBaseCommand.js";
 import {
   makeProcessRenderer,
   processFlags,
 } from "../../../rendering/process/process_flags.js";
-import { Flags } from "@oclif/core";
+import { Flags, ux } from "@oclif/core";
 import { Success } from "../../../rendering/react/components/Success.js";
 import {
   extensionManifestArg,
   parseExtensionManifest,
 } from "../../../lib/resources/extension/args_contributor.js";
+import { Text } from "ink";
+import { Value } from "../../../rendering/react/components/Value.js";
+import { ExtensionManifest } from "../../../lib/resources/extension/manifest.js";
+import { ProcessRenderer } from "../../../rendering/process/process.js";
 
 export default class Withdraw extends ExecRenderBaseCommand<
   typeof Withdraw,
@@ -19,6 +23,10 @@ export default class Withdraw extends ExecRenderBaseCommand<
 
   static flags = {
     ...processFlags,
+    force: Flags.boolean({
+      char: "f",
+      description: "Do not ask for confirmation",
+    }),
     reason: Flags.string({
       description: "Reason for withdrawal",
       required: true,
@@ -38,6 +46,10 @@ export default class Withdraw extends ExecRenderBaseCommand<
     );
     const { contributorId, id } = manifest;
 
+    if (!(await this.shouldProceed(p, manifest))) {
+      ux.exit(1);
+    }
+
     await p.runStep("Withdrawing extension", async () => {
       await this.apiClient.marketplace.extensionSetExtensionPublishedState({
         contributorId,
@@ -52,7 +64,44 @@ export default class Withdraw extends ExecRenderBaseCommand<
     await p.complete(<Success>Extension withdrawn successfully</Success>);
   }
 
+  protected async shouldProceed(
+    p: ProcessRenderer,
+    manifest: ExtensionManifest,
+  ): Promise<boolean> {
+    if (this.flags.force) {
+      return true;
+    }
+
+    const confirmed = await p.addConfirmation(
+      <TextConfirmWithdrawal manifest={manifest} />,
+    );
+    if (confirmed) {
+      return true;
+    }
+
+    p.addInfo(<TextWithdrawalCancelled manifest={manifest} />);
+    p.complete(<></>);
+
+    return false;
+  }
+
   protected render(): React.ReactNode {
     return undefined;
   }
 }
+
+const TextConfirmWithdrawal: FC<{ manifest: ExtensionManifest }> = ({
+  manifest,
+}) => (
+  <Text>
+    confirm withdrawal of extension <Value>{manifest.name}</Value>
+  </Text>
+);
+
+const TextWithdrawalCancelled: FC<{ manifest: ExtensionManifest }> = ({
+  manifest,
+}) => (
+  <Text>
+    withdrawal of extension <Value>{manifest.name}</Value> was cancelled
+  </Text>
+);
