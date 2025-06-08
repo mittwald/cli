cae419530e20aab9bdf09d9f8b78d6e0117ae4f3
Martin Helmich <m.helmich@mittwald.de>
2025-06-08 15:12:56 +0200

    feat: add support for env_file

--- src/lib/resources/stack/enrich.ts
+++ src/lib/resources/stack/enrich.ts
@@ -3,6 +3,12 @@ import {
   type MittwaldAPIV2,
   MittwaldAPIV2Client,
 } from "@mittwald/api-client";
+import { readFile } from "fs/promises";
+import { parse } from "envfile";
+import { ContainerServiceInput } from "./types.js";
+
+type ContainerServiceDeclareRequest =
+  MittwaldAPIV2.Components.Schemas.ContainerServiceDeclareRequest;
 
 type StackRequest =
   MittwaldAPIV2.Paths.V2StacksStackId.Put.Parameters.RequestBody;
@@ -15,38 +21,77 @@ export async function enrichStackDefinition(
   const enriched = structuredClone(input);
 
   for (const serviceName of Object.keys(input.services ?? {})) {
-    const service = enriched.services![serviceName];
+    let service = enriched.services![serviceName] as ContainerServiceInput;
 
-    const resp = await apiClient.container.getContainerImageConfig({
-      queryParameters: {
-        imageReference: service.image,
-        useCredentialsForProjectId: projectId,
-      },
-    });
+    service = await setCommandAndEntrypointFromImage(
+      apiClient,
+      projectId,
+      service,
+    );
 
-    assertStatus(resp, 200);
+    service = await setEnvironmentFromEnvFile(service);
 
-    if (service.ports === undefined) {
-      service.ports = (resp.data.exposedPorts ?? []).map((p) => p.port);
+    enriched.services![serviceName] = service as ContainerServiceDeclareRequest;
+  }
+
+  return enriched;
+}
+
+async function setEnvironmentFromEnvFile(
+  service: Readonly<ContainerServiceInput>,
+): Promise<ContainerServiceInput> {
+  if (!service.env_file) {
+    return service;
+  }
+
+  const enriched = structuredClone(service) as ContainerServiceInput;
+  const envFileContent = await readFile(service.env_file, "utf-8");
+  const envVars = parse(envFileContent);
+
+  delete enriched.env_file;
+  enriched.envs = {
+    ...envVars,
+    ...(enriched.envs ?? {}),
+  };
+
+  return enriched;
+}
+
+async function setCommandAndEntrypointFromImage(
+  apiClient: MittwaldAPIV2Client,
+  projectId: string,
+  service: Readonly<ContainerServiceInput>,
+): Promise<ContainerServiceInput> {
+  const enriched = structuredClone(service) as ContainerServiceDeclareRequest;
+  const resp = await apiClient.container.getContainerImageConfig({
+    queryParameters: {
+      imageReference: service.image,
+      useCredentialsForProjectId: projectId,
+    },
+  });
+
+  assertStatus(resp, 200);
+
+  if (service.ports === undefined) {
+    enriched.ports = (resp.data.exposedPorts ?? []).map((p) => p.port);
+  }
+
+  if (service.command === undefined) {
+    let command = resp.data.command;
+    if (typeof command === "string") {
+      command = [command];
     }
 
-    if (service.command === undefined) {
-      let command = resp.data.command;
-      if (typeof command === "string") {
-        command = [command];
-      }
+    enriched.command = command;
+  }
 
-      service.command = command;
+  if (service.entrypoint === undefined) {
+    let entrypoint = resp.data.entrypoint;
+    if (typeof entrypoint === "string") {
+      entrypoint = [entrypoint];
     }
 
-    if (service.entrypoint === undefined) {
-      let entrypoint = resp.data.entrypoint;
-      if (typeof entrypoint === "string") {
-        entrypoint = [entrypoint];
-      }
-
-      service.entrypoint = entrypoint;
-    }
+    enriched.entrypoint = entrypoint;
   }
 
   return enriched;
