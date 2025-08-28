878a433a15174314bbdfd63d12e418b48d1ac28c
Lukas Fritze <l.fritze@mittwald.de>
2025-08-28 15:56:49 +0200

    Add prerun hook for update: Check if installed with brew
    relates https://github.com/mittwald/cli/issues/1322

---
+++ src/hooks/prerun/update-brew-check.ts
@@ -0,0 +1,24 @@
+import { Hook } from "@oclif/core";
+import { execSync } from "child_process";
+
+const hook: Hook<"prerun"> = async function (opts) {
+  const isInstalledWithBrew = () => {
+    try {
+      const cellar = execSync("brew --cellar", { encoding: "utf8" });
+      return opts.config.root.startsWith(cellar);
+    } catch {
+      return false;
+    }
+  };
+
+  if (opts.Command.id === "update") {
+    if (isInstalledWithBrew()) {
+      opts.context.warn(
+        "installed with brew.\nUse `brew upgrade mw` to update to the newest version",
+      );
+      opts.context.exit(1);
+    }
+  }
+};
+
+export default hook;
