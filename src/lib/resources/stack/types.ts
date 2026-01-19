5fb5f67fce8911bc765910a83c32194e7468bcba
Lars Bergmann <L.Bergmann@mittwald.de>
2026-01-19 14:40:18 +0100

    introduce new type for raw stack input

--- src/lib/resources/stack/types.ts
+++ src/lib/resources/stack/types.ts
@@ -12,3 +12,16 @@ export type ContainerServiceInput = ContainerServiceDeclareRequest & {
   };
   ports?: string[];
 };
+
+// We've kept this as simple and flexible as possible to reduce bloat
+export type RawStackInput = {
+  services: {
+    [serviceName: string]: {
+      // Allow raw input and map notation for environment vars
+      environment?: string[] | { [k: string]: string };
+      env_file?: string | string[];
+      image: string;
+      ports?: string[];
+    };
+  };
+};
