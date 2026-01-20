0e2d4da64ebccc3119d9bd52f530208393018ea4
Lars Bergmann <L.Bergmann@mittwald.de>
2026-01-20 15:27:51 +0100

    preliminary fix for broken env file parser

--- src/lib/resources/stack/env.ts
+++ src/lib/resources/stack/env.ts
@@ -1,8 +1,21 @@
 import * as fs from "fs/promises";
-import { parse } from "envfile";
 import { pathExists } from "../../util/fs/pathExists.js";
 import { ProcessRenderer } from "../../../rendering/process/process.js";
 import { getRandomValues } from "node:crypto";
+
+export const parse = (src: string): Record<string, string> => {
+  const result: Record<string, string> = {};
+  const lines = src.toString().split("\n");
+  for (const line of lines) {
+    const match = line.match(/^([^=:#]+?)[=:](.*)/);
+    if (match) {
+      const key = match[1].trim();
+      const value = match[2].trim(); // Modify this to preserve quotes
+      result[key] = value; // Retain the original value without removing quotes
+    }
+  }
+  return result;
+};
 
 export async function collectEnvironment(
   base: NodeJS.ProcessEnv,
