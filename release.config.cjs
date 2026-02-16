903818d95a5bbe690406a62fbb4d6914a36a5afd
Martin Helmich <m.helmich@mittwald.de>
2026-02-16 15:13:09 +0100

    chore(ci): formatting

--- release.config.cjs
+++ release.config.cjs
@@ -7,7 +7,8 @@ module.exports = {
     [
       "@semantic-release/git",
       {
-        message: `chore(release): \${nextRelease.version}\n\n\${nextRelease.notes}`,
+        message:
+          "chore(release): ${nextRelease.version}\n\n${nextRelease.notes}",
       },
     ],
   ],
