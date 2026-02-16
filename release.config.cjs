66062c7fd6323a87b78941acdf75719c87f01bf6
Martin Helmich <m.helmich@mittwald.de>
2026-02-16 16:22:42 +0100

    chore(release): write CHANGELOG automatically

--- release.config.cjs
+++ release.config.cjs
@@ -4,6 +4,7 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
     [
       "@semantic-release/git",
       {
